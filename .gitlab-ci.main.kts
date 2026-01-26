4cd42c65af610bc8528f2be0d67914af92d47e70
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-01-26 11:49:45 +0100

    ci(gitlab): Fix static copy

--- .gitlab-ci.main.kts
+++ .gitlab-ci.main.kts
@@ -70,7 +70,7 @@ gitlabCi {
 			script {
 				shell("mkdir -p public")
 				shell("mv -T docs-website public")
-				shell("mv -T docs/website/static public")
+				shell("cp -a docs/website/static/. public")
 			}
 
 			artifacts {
